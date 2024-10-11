fd6dea6957d859efc796b7b83c20301aebc22c3a
Timur <timurrahmatullaev703@gmail.com>
2024-10-11 18:08:36 +0600

    rename Field to Cell

---
+++ Cell.java
@@ -0,0 +1,33 @@
+﻿public class Cell {
+
+    public boolean isHit = false;
+    public boolean hasShip = false;
+
+    public void hit() {
+        this.isHit = true;
+    }
+    public void placeShip() {
+        this.hasShip = true;
+    }
+    public String getStatusSign() {
+        String sign = "";
+
+        if(this.isHit && this.hasShip) {
+
+            sign = "--";
+            
+        } else if(this.isHit) {
+
+            sign = "**";
+
+        } else if(this.hasShip) {
+
+            sign = "XX";
+
+        } else {
+            sign = "[]";
+        }
+
+        return sign;
+    }
+}
