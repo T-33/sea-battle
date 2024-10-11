6743d467547ad73d068db242378994ff34e6aeca
Timur <timurrahmatullaev703@gmail.com>
2024-10-11 18:09:13 +0600

    add GameField class

---
+++ GameField.java
@@ -0,0 +1,102 @@
+﻿public class GameField {
+
+    private int rowsNumber;
+    private int columnsNumber;
+
+    public Cell[][] gameField;
+
+    private static final String[] coordinateLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+    public GameField(int rows, int columns) {
+        this.rowsNumber = rows;
+        this.columnsNumber = columns;
+
+        this.gameField = new Cell[rows][columns];
+
+
+// initialising each field;
+        for (int i = 0; i < rowsNumber; i++) {
+
+            for (int j = 0; j < columnsNumber; j++) {
+
+                this.gameField[i][j] = new Cell();
+
+            }
+
+        }
+
+    }
+
+    public int getRowsNumber() {
+        return this.rowsNumber;
+    }
+
+    public int getColumnsNumber() {
+        return this.columnsNumber;
+    }
+
+    public void hitCell (int x, int y) {
+        this.gameField[x - 1][y - 1].hit();
+    }
+
+    public void placeShip(int shipLength) {
+
+        int min = 0;
+		int max = 9;
+
+        boolean isFixedX = (1 + (int)(Math.random() * ((2 - 1) + 1))) == 1;
+
+        int xRand = min + (int)(Math.random() * ((max - min) + 1));
+        int yRand = min + (int)(Math.random() * ((max - min) + 1));
+
+        // if(xRand == 0 || xRand == 9) {
+        //     isFixedX = false;
+        // }
+        // if(yRand == 0 || yRand == 9) {
+        //     isFixedX = true;
+        // }
+
+        for(int i = 0; i < shipLength; i += 1) {
+
+            while(true) {
+
+                if(!this.gameField[xRand][yRand].hasShip) {
+
+                    this.gameField[xRand][yRand].placeShip();
+                    break;
+                } 
+
+                if(isFixedX) {
+                    yRand = Math.abs(yRand - 1);
+                } else {
+                    xRand = Math.abs(xRand - 1);
+                }
+                
+
+            }
+    }
+
+    }
+
+    public void printField() {
+        for (int i = 0; i < rowsNumber; i++) {
+
+            for (int j = 0; j < columnsNumber; j++) {
+        
+                 System.out.print(this.gameField[i][j].getStatusSign() + " ");
+                            
+            }
+
+            System.out.println("\n");
+        }
+    }
+
+    public boolean isCellAvailable(int x, int y) {
+        boolean isXNeighboursAvailable = x > 0 && x < 9 && !(this.gameField[x - 1][y].hasShip && this.gameField[x + 1][y].hasShip);
+        boolean isYNeighboursAvailable = y > 0 && y < 9 && !(this.gameField[x][y - 1].hasShip && this.gameField[x][y + 1].hasShip);
+
+        return isXNeighboursAvailable && isYNeighboursAvailable;
+
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -2,127 +2,103 @@
 import java.util.Scanner;
 
 
-class Field {
 
-    public boolean isHit = false;
-    public boolean hasShip = false;
-
-    public void hit() {
-        this.isHit = true;
-    }
-    public void placeShip() {
-        this.hasShip = true;
-    }
-    public String getStatusSign() {
-        String sign = "";
-
-        if(this.isHit && this.hasShip) {
-            sign = "--";
-        } else if(this.isHit) {
-
-            sign = "**";
-
-        } else if(this.hasShip) {
-
-            sign = "XX";
-
-        } else {
-            sign = "[]";
-        }
-
-        return sign;
-    }
-}
 public class Main
 {
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        int rowsNumber = scanner.nextInt();
-        int columnsNumber = scanner.nextInt();
+        GameField gameField = new GameField(10, 10);
+
+        gameField.placeShip(4);
+        gameField.placeShip(3);
+        gameField.placeShip(1);
+        gameField.placeShip(1);
+        gameField.placeShip(1);
+        gameField.placeShip(1);
+
+
+        gameField.printField();
         
-        String emptyFieldSign = "\u26F6 ";
-
-        Field[][] myIntMatrix = new Field[rowsNumber] [columnsNumber];
-
-        String[] coordinateLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
-
-        for (int i = 0; i < rowsNumber; i++) {
-
-            for (int j = 0; j < columnsNumber; j++) {
-
-                myIntMatrix[i][j] = new Field();
-
-            }
-
-        }
-
-        // for (int i = 0; i < rowsNumber; i++) {
-
-        //     for (int j = 0; j < columnsNumber; j++) {
-
-
-        //         if(myIntMatrix[i][j].isHit) {
-
-        //             System.out.print("$x$ ");
-
-        //         } else   System.out.print(coordinateLetters[i] + "x" + (j + 1) + " ");
-                
-        //     }
-        //     System.out.println("\n");
-        // }
-
-        int min = 0;
-		int max = 9;
-
-
-        for(int i = 0; i < 20; i += 1) {
-
-                while(true) {
-
-                    int xRand = min + (int)(Math.random() * ((max - min) + 1));
-                    int yRand = min + (int)(Math.random() * ((max - min) + 1));
-
-                    if(!myIntMatrix[xRand][yRand].hasShip) {
-
-                        myIntMatrix[xRand][yRand].placeShip();
-                        break;
-                    }
-                }
-		}
-
-        for (int i = 0; i < rowsNumber; i++) {
-
-            for (int j = 0; j < columnsNumber; j++) {
-
-                if(myIntMatrix[i][j].hasShip) {
-
-                    System.out.print("ship ");
-
-                } else   System.out.print(coordinateLetters[i] + "x" + (j + 1) + " ");
-                
-            }
-            System.out.println("\n");
-        }
 
         while(true) {
+            int userX = sc.nextInt();
+            int userY = sc.nextInt();
 
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
+            gameField.hitCell(userX, userY);
 
-            myIntMatrix[x - 1][y - 1].hit();
-
-            for (int i = 0; i < rowsNumber; i++) {
-
-                for (int j = 0; j < columnsNumber; j++) {
-
-                    System.out.print(myIntMatrix[i][j].getStatusSign() + " ");
-                    
-                }
-                System.out.println("\n");
-            }
-
+            gameField.printField();
         }
-	}
+  
+
+
+    //     // for(int i = 0; i < 20; i += 1) {
+
+    //     //         while(true) {
+
+    //     //             int xRand = min + (int)(Math.random() * ((max - min) + 1));
+    //     //             int yRand = min + (int)(Math.random() * ((max - min) + 1));
+
+    //     //             if(!myIntMatrix[xRand][yRand].hasShip) {
+
+    //     //                 myIntMatrix[xRand][yRand].placeShip();
+    //     //                 break;
+    //     //             }
+    //     //         }
+	// 	// }
+
+    //     placeShip(4, myIntMatrix);
+
+    //     for (int i = 0; i < rowsNumber; i++) {
+
+    //         for (int j = 0; j < columnsNumber; j++) {
+
+    //             if(myIntMatrix[i][j].hasShip) {
+
+    //                 System.out.print("ship ");
+
+    //             } else   System.out.print(coordinateLetters[i] + "x" + (j + 1) + " ");
+                
+    //         }
+    //         System.out.println("\n");
+    //     }
+
+    //     while(true) {
+
+    //         int x = scanner.nextInt();
+    //         int y = scanner.nextInt();
+
+    //         myIntMatrix[x - 1][y - 1].hit();
+
+    //         for (int i = 0; i < rowsNumber; i++) {
+
+    //             for (int j = 0; j < columnsNumber; j++) {
+
+    //                 System.out.print(myIntMatrix[i][j].getStatusSign() + " ");
+                    
+    //             }
+    //             System.out.println("\n");
+    //         }
+    //     }
+	// }
+    // // changes gameField from main since placeShips gets pointer to gameField;
+  
+
+    // public static void clearConsole() {  
+    //     System.out.print("\033[H\033[2J");  
+    //     System.out.flush();  
+    // }
+    
+    // // private static Field[][] placeShip(int shipSize, Field[][])
+
+    // // private static boolean isFieldFree(int x, int y, Field[][] gameField) {
+
+    // //     if( gameField[x + 1][y].hasShip || gameField[x][y + 1].hasShip || 
+    // //         gameField[x - 1][y].hasShip || gameField[x][y - 1].hasShip 
+    // //         ) {
+            
+    // //     }
+        
+     }
 }
