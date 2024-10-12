4081407da68bd95390c9be45695e0e5244bd9a2e
Timur <timurrahmatullaev703@gmail.com>
2024-10-12 20:48:28 +0600

    rework ships placement and rename to SeaBattle

--- Main.java
+++ Main.java
@@ -9,14 +9,21 @@ public class Main
 
 		Scanner sc = new Scanner(System.in);
 
-        GameField gameField = new GameField(10, 10);
+        SeaBattle gameField = new SeaBattle(10, 10);
 
         gameField.placeShip(4);
         gameField.placeShip(3);
+        gameField.placeShip(3);
+        gameField.placeShip(2);
+        gameField.placeShip(2);
+        gameField.placeShip(2);
         gameField.placeShip(1);
         gameField.placeShip(1);
         gameField.placeShip(1);
         gameField.placeShip(1);
+
+
+
 
 
         gameField.printField();
@@ -30,75 +37,5 @@ public class Main
 
             gameField.printField();
         }
-  
-
-
-    //     // for(int i = 0; i < 20; i += 1) {
-
-    //     //         while(true) {
-
-    //     //             int xRand = min + (int)(Math.random() * ((max - min) + 1));
-    //     //             int yRand = min + (int)(Math.random() * ((max - min) + 1));
-
-    //     //             if(!myIntMatrix[xRand][yRand].hasShip) {
-
-    //     //                 myIntMatrix[xRand][yRand].placeShip();
-    //     //                 break;
-    //     //             }
-    //     //         }
-	// 	// }
-
-    //     placeShip(4, myIntMatrix);
-
-    //     for (int i = 0; i < rowsNumber; i++) {
-
-    //         for (int j = 0; j < columnsNumber; j++) {
-
-    //             if(myIntMatrix[i][j].hasShip) {
-
-    //                 System.out.print("ship ");
-
-    //             } else   System.out.print(coordinateLetters[i] + "x" + (j + 1) + " ");
-                
-    //         }
-    //         System.out.println("\n");
-    //     }
-
-    //     while(true) {
-
-    //         int x = scanner.nextInt();
-    //         int y = scanner.nextInt();
-
-    //         myIntMatrix[x - 1][y - 1].hit();
-
-    //         for (int i = 0; i < rowsNumber; i++) {
-
-    //             for (int j = 0; j < columnsNumber; j++) {
-
-    //                 System.out.print(myIntMatrix[i][j].getStatusSign() + " ");
-                    
-    //             }
-    //             System.out.println("\n");
-    //         }
-    //     }
-	// }
-    // // changes gameField from main since placeShips gets pointer to gameField;
-  
-
-    // public static void clearConsole() {  
-    //     System.out.print("\033[H\033[2J");  
-    //     System.out.flush();  
-    // }
-    
-    // // private static Field[][] placeShip(int shipSize, Field[][])
-
-    // // private static boolean isFieldFree(int x, int y, Field[][] gameField) {
-
-    // //     if( gameField[x + 1][y].hasShip || gameField[x][y + 1].hasShip || 
-    // //         gameField[x - 1][y].hasShip || gameField[x][y - 1].hasShip 
-    // //         ) {
-            
-    // //     }
-        
      }
 }
---
+++ SeaBattle.java
@@ -0,0 +1,124 @@
+﻿public class SeaBattle {
+
+    private int rowsNumber;
+    private int columnsNumber;
+
+    private Cell[][] gameField;
+
+    private static final String[] coordinateLetters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+    public SeaBattle(int rows, int columns) {
+        this.rowsNumber = rows;
+        this.columnsNumber = columns;
+
+        gameField = new Cell[rows][columns];
+
+        // initialising each field;
+        for (int i = 0; i < rowsNumber; i++) {
+            for (int j = 0; j < columnsNumber; j++) {
+                gameField[i][j] = new Cell();
+            }
+        }
+    }
+
+    public int getRowsNumber() {
+        return rowsNumber;
+    }
+
+    public int getColumnsNumber() {
+        return columnsNumber;
+    }
+
+    public void hitCell(int x, int y) {
+        gameField[x][y].hit();
+    }
+
+    public void placeShip(int shipSize) {
+        boolean isHorizontal = (1 + (int) (Math.random() * ((2 - 1) + 1))) == 1;
+
+        int min = 0;
+        int max = 9;
+
+        int i = min + (int) (Math.random() * ((max - min) + 1));
+        int j = min + (int) (Math.random() * ((max - min) + 1));
+
+        while (true) {
+            for (; i <= 9; i++) {
+                outerloop: for (; j <= 9; j++) {
+                    if (isHorizontal) {
+                        for (int k = 0; k < shipSize; k++) {
+                            if ((j + k) > 9 || !isCellAvailable(i, j + k))
+                                continue outerloop;
+                        }
+                        for (int k = 0; k < shipSize; k++) {
+                            gameField[i][j + k].placeShip();
+                        }
+                        return;
+                    } else {
+                        for (int k = 0; k < shipSize; k++) {
+                            if ((i + k) > 9 || !isCellAvailable(i + k, j))
+                                continue outerloop;
+                        }
+                        for (int k = 0; k < shipSize; k++) {
+                            gameField[i + k][j].placeShip();
+                        }
+                        return;
+                    }
+                }
+                j = 0;
+            }
+
+            i = min + (int) (Math.random() * ((max - min) + 1));
+
+            isHorizontal = isHorizontal ? false : true;
+        }
+
+    }
+
+    public void printField() {
+        for (int i = 0; i < rowsNumber; i++) {
+
+            for (int j = 0; j < columnsNumber; j++) {
+
+                System.out.print(gameField[i][j].getStatusSign() + "");
+            }
+
+            System.out.println("\n");
+        }
+    }
+
+    private boolean isCellAvailable(int x, int y) {
+        boolean hasShip = gameField[x][y].hasShip;
+
+        boolean isXNeighboursAvailable = !((x > 0 && x < 9)
+                && (gameField[x - 1][y].hasShip || gameField[x + 1][y].hasShip) || hasShip);
+        boolean isYNeighboursAvailable = !((y > 0 && y < 9)
+                && (gameField[x][y - 1].hasShip || gameField[x][y + 1].hasShip) || hasShip);
+
+        // checking cells in the corners
+        if (x == 0 && y == 0) {
+            return !(gameField[x][y + 1].hasShip || gameField[x + 1][y].hasShip || hasShip);
+        } else if (x == 0 && y == 9) {
+            return !(gameField[x][y - 1].hasShip || gameField[x + 1][y].hasShip || hasShip);
+        } else if (x == 9 && y == 0) {
+            return !(gameField[x][y + 1].hasShip || gameField[x - 1][y].hasShip || hasShip);
+        } else if (x == 9 && y == 9) {
+            return !(gameField[x][y - 1].hasShip || gameField[x - 1][y].hasShip || hasShip);
+        }
+        // checking cells by sides
+        else if (x == 0) {
+            return !(gameField[x][y - 1].hasShip || gameField[x][y + 1].hasShip || gameField[x + 1][y].hasShip
+                    || hasShip);
+        } else if (x == 9) {
+            return !(gameField[x][y - 1].hasShip || gameField[x][y + 1].hasShip || gameField[x - 1][y].hasShip
+                    || hasShip);
+        } else if (y == 0) {
+            return !(gameField[x - 1][y].hasShip || gameField[x + 1][y].hasShip || gameField[x][y + 1].hasShip
+                    || hasShip);
+        } else if (y == 9) {
+            return !(gameField[x - 1][y].hasShip || gameField[x + 1][y].hasShip || gameField[x][y - 1].hasShip
+                    || hasShip);
+        } else
+            return isXNeighboursAvailable && isYNeighboursAvailable; // return availability for inner cells
+    }
+}
