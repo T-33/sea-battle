ea167d16247c097929d6913f632120ff96bb3c8d
Timur <timurrahmatullaev703@gmail.com>
2024-10-14 19:57:40 +0600

    add sunk method for Cell

--- Cell.java
+++ Cell.java
@@ -1,14 +1,21 @@
 ﻿public class Cell {
+    //TODO make every instance variable private 
+    //add getters and setters
 
     public boolean isHit = false;
     public boolean hasShip = false;
-    public boolean isSunken = false;
+    private boolean isSunken = false;
 
     public void hit() {
         isHit = true;
     }
     public void placeShip() {
         hasShip = true;
+    }
+    public void sunk() {
+        if(hasShip) {
+            isSunken = true;
+        }
     }
     public void cleanCell() {
         hasShip = false;
