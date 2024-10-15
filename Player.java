76722a07461c36dcff51322812d7713b60d7bdcb
Timur <timurrahmatullaev703@gmail.com>
2024-10-15 11:00:49 +0600

    add Player class

---
+++ Player.java
@@ -0,0 +1,19 @@
+﻿public class Player {
+    
+    private String name;
+    private int hits;
+
+    public Player(String name, int hits) {
+        this.name = name;
+        this.hits = hits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+}
