554f5a7028cf4a50bb85e4d95b687759334f94c6
Timur <timurrahmatullaev703@gmail.com>
2024-10-18 21:43:03 +0600

    add scoreBoard

--- Main.java
+++ Main.java
@@ -17,25 +17,8 @@ public class Main
         gameField.init(shipsSize);
 
         
-        while(true) {
-            gameField.printField();
-            System.out.print("\n");
-            System.out.println("Please enter which field you want to hit (in following format: either  A 1 or 1 1):");
-            String userX = sc.nextLine();
-
-            // gameField.hitCell(userX);
-            cleanScreen();
-            gameField.hitCell(userX);
-            cleanScreen();
-
-            // 
-
-            gameField.printField();
-        }
+        
      }
 
-     public static void cleanScreen() {
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();  
-     }
+
 }
